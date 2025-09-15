a818c57b803007fade324ad7e9ffb553eb38613f
Thomah6 <agapethounk@gmail.com>
2025-09-15 20:46:13 +0100

    RouterLink, Routes et creations des views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AdminView from '@/views/AdminView.vue'
+import CatalogView from '@/views/CatalogView.vue'
+import CourseView from '@/views/CourseView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+    path: '/',
+    name: 'home_catalogue',
+    component: CatalogView,
+    meta: { title: 'Accueil - DeepLearn' }
+  },
+  {
+    path: '/catalog',
+    name: 'catalog',
+    component: CatalogView,
+    meta: { title: 'Catalogue - DeepLearn' }
+  },
+  {
+    path: '/course/:id',
+    name: 'course',
+    component: CourseView,
+    props: true,
+    meta: { title: 'Cours - DeepLearn' }
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+    meta: { title: 'Profil - DeepLearn', requiresAuth: true }
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView,
+    meta: { title: 'Administration - DeepLearn', requiresAuth: true, isAdmin: true }
+  }
+  ],
 })
 
 export default router
