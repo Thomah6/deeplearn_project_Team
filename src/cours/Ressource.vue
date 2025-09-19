416c1974db5917ad86a87adab4fa8d631df611ef
Houéfa Nadège DJOSSOU <nadge.djossou@chem.loc>
2025-09-19 20:14:18 +0200

    quizz

--- src/cours/Ressource.vue
+++ src/cours/Ressource.vue
@@ -68,14 +68,10 @@ watch(() => props.lesson, (newLesson) => {
       </button>
     </div>
 
-    <!-- Message de progression complète -->
-    <!-- <div v-if="progress === 100" class="mt-4 p-3 bg-green-100 dark:bg-green-900 text-green-800 dark:text-green-200 rounded-lg">
-      <p class="text-center">Félicitations ! Leçon terminée ✅</p>
-      <p v-if="props.progressComplete" class="text-center text-sm mt-2">Passage à la leçon suivante...</p>
-    </div> -->
-<div class="mt-4 p-3 bg-blue-100 dark:bg-blue-900 text-blue-800 dark:text-blue-200 rounded-lg text-center">
+    
+<div class="mt-4 p-3 bg-blue-500 text-white dark:bg-blue-900 text-blue-800 dark:text-blue-200 rounded-lg text-center">
   
-  <RouterLink :to="{name:'quizz' , params : {id:route.params.id}}"><button class="border text-white">Commencer le quiz</button></RouterLink>
+  <RouterLink :to="{name:'quizz' , params : {id:route.params.id}}"><button class="text-white">Commencer le quiz</button></RouterLink>
 
 </div>
   </section>
